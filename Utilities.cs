0ca9da4180610f338617ac051e9c221c21804a2a
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-05-18 12:25:34 +0200

    Draft Web UI structure

---
+++ Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace SakuraIsayeki.Screener;
+
+public static class Utilities
+{
+	public static async Task<ulong?> GetUserSnowflakeAsync(this AuthenticationStateProvider authenticationStateProvider) 
+		=> (await authenticationStateProvider.GetAuthenticationStateAsync()).User is { } user
+			&& user.FindFirst(ClaimTypes.NameIdentifier) is { } claim
+			&& ulong.TryParse(claim.Value, out ulong snowflake)
+				? snowflake
+				: null;
+}
